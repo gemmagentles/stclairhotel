25b7010bd161bc440a9e3f5eb623cb596a9ec3a5
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-04-29 17:21:28 -0700

    make slider responsive.

--- wp-content/themes/stclair/js/highlight-slider.js
+++ wp-content/themes/stclair/js/highlight-slider.js
@@ -4,10 +4,27 @@ if (highlightSlider.length > 0) {
   var slider = tns({
     container: '.highlight-slider',
     items: 1,
-    edgePadding: 222,
+    edgePadding: 0,
     swipeAngle: false,
     speed: 700,
-    gutter: 44,
-    nav: true
+    gutter: 0,
+    nav: true,
+    responsive: {
+      600: {
+        edgePadding: 80,
+        gutter: 20
+      },
+      768: {
+        edgePadding: 120,
+        gutter: 30
+      },
+      1120: {
+        edgePadding: 222,
+        gutter: 44
+      },
+      1582: {
+        edgePadding: 322
+      }
+    }
   });
 }
--- wp-content/themes/stclair/partials/highlight-slider.php
+++ wp-content/themes/stclair/partials/highlight-slider.php
@@ -5,16 +5,15 @@
             <?php $slide_image = get_sub_field( 'slide_image' ); ?>
             <?php if ( $slide_image ) { ?>
                 <div>
+                    <img class="highlight-slider__slide-image" src="<?php echo $slide_image['url']; ?>" alt="<?php echo $slide_image['alt']; ?>" />
                     <div class="highlight-slider__info">
                         <h4 class="highlight-slider__info--title"><?php the_field( 'caption' ); ?></h4>
                         <div class="highlight-slider__progress-nav">
-                            <!-- <div class="tns-controls" aria-label="Carousel Navigation" tabindex="0"><button data-controls="prev" tabindex="-1" aria-controls="tns2">prev</button><button data-controls="next" tabindex="-1" aria-controls="tns2">next</button></div> -->
                             <!-- check how many slides there are to add the number in the progress bar end -->
                             <?php $count = count(get_field( 'highlight_slides' )); ?>
                             <span class="highlight-slider__progress-nav--current-number">0<?php echo get_row_index(); ?></span><div class="highlight-slider__progress-nav--line"><span></span></div><span class="highlight-slider__progress-nav--total-number">0<?php echo $count; ?></span>
                         </div>
                     </div>
-                    <img class="highlight-slider__slide-image" src="<?php echo $slide_image['url']; ?>" alt="<?php echo $slide_image['alt']; ?>" />
                 </div>
             <?php } ?>
         <?php endwhile; ?>
