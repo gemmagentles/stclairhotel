12d75c4652fb4ddfa44b7769fa316c688cc26071
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-05-14 15:42:10 -0700

    create a 2nd version of wysiwyg component so it can be used twice on the same page but also have a slider inbetween to break the layout. Create a version of cards component for news, so the hero values of the hero posts can be pulled in dynamically, and the user does not have to fill in the cards. Create a hero for the news page and the individual news posts so that no duplicate content gets pulled in as they all have different variable names. Create a wpb single post php file to give the posts a unique template.

--- wp-content/themes/stclair/partials/hero.php
+++ wp-content/themes/stclair/partials/hero.php
@@ -6,6 +6,16 @@
             <div class="hero__container">
 
                 <div class="hero__content">
+                    <?php
+                        if ( is_single() ) {
+                        // wp_enqueue_style( string $handle, string $src = '', array $deps = array(), string|bool|null $ver = false, string $media = 'all' )
+                        ?>
+                        <!-- post details -->
+                        <span class="hero__blog-date"><?php the_time('F j, Y'); ?> </span>
+                        <!-- /post details -->
+                        <?php
+                        }
+                    ?>
                     <h1 class="hero__content--heading"><?php the_sub_field( 'hero_heading' ); ?></h1>
                     <p class="hero__content--paragraph"><?php the_sub_field( 'hero_paragraph' ); ?></p>
                 </div>
---
+++ wp-content/themes/stclair/partials/news-cards.php
@@ -0,0 +1,18 @@
+<div class="cards__individual-card-wrapper">
+    <?php if ( have_rows( 'news_posts_hero' ) ) : ?>
+        <?php while ( have_rows( 'news_posts_hero' ) ) : the_row(); ?>
+            <?php if ( get_sub_field( 'news_posts_hero_background_image' ) ) { ?>
+                <img class="cards__image" src="<?php the_sub_field( 'news_posts_hero_background_image' ); ?>" />
+            <?php } ?>
+            <div class="cards__individual-card-inner">
+                <h3 class="cards__title"><?php the_sub_field( 'news_posts_hero_heading' ); ?></h3>
+            </div>
+            <div class="cards__individual-card--paragraph">
+                <?php the_sub_field( 'news_posts_hero_paragraph' ); ?>
+            </div>
+            <div class="cards__individual-card--button">
+                <button class="cards__button stclair-button" onclick="location.href='<?php the_permalink(); ?>'" type="button">Read More</button>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+</div>
