566b724f493051b161bc5685506d7e642c0af5f1
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-12-19 12:59:36 -0800

    Add black overlay to all sliders that arent on the home page so the white text is readable. Also make sure the p tag and diamond images only show up if there selected in the cms, so the spacing is always correct.

--- wp-content/themes/stclair/partials/hero-slider.php
+++ wp-content/themes/stclair/partials/hero-slider.php
@@ -2,11 +2,12 @@
 <div class="hero-slider__wrapper">
     <div class="hero-slider__content">
         <div class="hero-slider__content--inner" data-aos="fade-up">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/diamond-divider.svg" alt="" class="hero-slider__content--diamond-divider">
             <!-- Logo overlay -->
             <?php $logo = get_field( 'logo' ); ?>
             <?php if ( $logo ) { ?>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/icons/diamond-divider.svg" alt="" class="hero-slider__content--diamond-divider">
                 <img class="hero-slider__content--logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" />
+                <img src="<?php echo get_template_directory_uri(); ?>/img/icons/diamond-divider.svg" alt="" class="hero-slider__content--diamond-divider">
             <?php } ?>
             <!-- Text overlay -->
             <?php $heading = get_field( 'heading' ); ?>
@@ -17,8 +18,10 @@
             <?php if ( $subheading ) { ?>
                 <h2 class="hero-slider__content--sub-heading"><?php the_field( 'sub_heading' ); ?></h2>
             <?php } ?>
-            <p class="hero-slider__content--text"><?php the_field( 'text' ); ?></p>
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icons/diamond-divider.svg" alt="" class="hero-slider__content--diamond-divider">
+            <?php $paragraph = get_field( 'text' ); ?>
+            <?php if ( $paragraph ) { ?>
+                <p class="hero-slider__content--text"><?php the_field( 'text' ); ?></p>
+            <?php } ?>
         </div>
     </div>
     <div class="hero-slider__overlay"></div>
