4149856f77792a30eef0668340643d9357384e81
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-12-19 17:29:10 -0800

    Add a pop up modal to reserve now buttons in 3 different places. Make it loop to make all 3 work on one page. This is while the client is waiting on book engine.

--- wp-content/themes/stclair/src/components/header.js
+++ wp-content/themes/stclair/src/components/header.js
@@ -9,6 +9,36 @@
         $(".nav__hamburger-icon").click(function(){
         $(".nav__hamburger-icon").toggleClass("nav__open");
     });
+    
+    // Modal Pop Up when Reserve Now button clicked
+
+    var btn = document.getElementsByClassName("click-to-open");
+
+    for (var i = 0; i < btn.length; i++) {
+    var thisBtn = btn[i];
+    thisBtn.addEventListener("click", function(){
+
+    // Open
+        var modal = document.getElementById(this.dataset.modal);
+        modal.style.display = "block";
+
+    // Close 
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+            modal.style.display = "none";
+            }
+        }
+
+    }, false)};
 
 });
 
